e53fb73a1927c59f4835cce7be3e8b49020b3c0a
keigo.hattori <keigoht@gmail.com>
2017-02-26 00:43:12 +0900

    簡単ツイート収集APIの機能拡張。

---
+++ twitter-response/src/main/java/com/apitore/banana/response/twitter/sample/Api23TwitterMyTweetExample.java
@@ -0,0 +1,35 @@
+package com.apitore.banana.response.twitter.sample;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.apitore.banana.response.twitter.TwitterResponseEntity;
+import com.apitore.banana.utils.UrlFormatter;
+
+
+/**
+ * @author Keigo Hattori
+ */
+public class Api23TwitterMyTweetExample {
+
+  static String ENDPOINT     = "https://api.apitore.com/api/23/twitter/mytweet";
+  static String ACCESS_TOKEN = "YOUR-ACCESS-TOKEN";
+
+  public static void main(String[] args) {
+    RestTemplate restTemplate = new RestTemplate();
+    Map<String, String> params = new HashMap<String, String>();
+    params.put("access_token", ACCESS_TOKEN);
+    params.put("iter", "1");
+    String url = UrlFormatter.format(ENDPOINT, params);
+
+    TwitterResponseEntity response =
+        restTemplate.getForObject(url, TwitterResponseEntity.class, params);
+
+    System.out.println(response.getLog());
+    System.out.println(response.getTweets().get(0).getText());
+  }
+
+}
